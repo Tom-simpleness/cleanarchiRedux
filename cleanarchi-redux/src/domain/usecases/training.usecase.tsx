ce950465c455cb76a0a95417f9489166534cea50
ToLan <t.landes84@gmail.com>
2024-10-11 12:16:33 +0200

    Add Redux

---
+++ cleanarchi-redux/src/domain/usecases/training.usecase.tsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const trainingSlice = createSlice({
+  name: "training",
+  initialState: {},
+  reducers: {},
+  extraReducers: () => {},
+});
+
+export const trainingReducer = trainingSlice.reducer;
---
+++ cleanarchi-redux/src/domain/usecases/trainings.usecase.tsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const trainingsSlice = createSlice({
+  name: "trainings",
+  initialState: {},
+  reducers: {},
+  extraReducers: () => {},
+});
+
+export const trainingsReducer = trainingsSlice.reducer;
