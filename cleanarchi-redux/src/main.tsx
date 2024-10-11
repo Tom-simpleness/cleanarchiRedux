5ed9eb8e017870f54c0a5c23c62cda7ffdee0f38
ToLan <t.landes84@gmail.com>
2024-10-11 12:48:01 +0200

    Add chackra UI

--- cleanarchi-redux/src/main.tsx
+++ cleanarchi-redux/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./utils/ui-theme/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </StrictMode>
 );
--- cleanarchi-redux/src/userinterface/pages/dashboard/Dashboard.tsx
+++ cleanarchi-redux/src/userinterface/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,39 @@
-export const Dashboard = () => {
-  return <div>Dashboard</div>;
+import { Container, Grid, Heading } from "@chakra-ui/react";
+
+export const DashboardPage = () => {
+  return (
+    <>
+      <Container
+        position="relative"
+        bg="gray.800"
+        borderRadius="2rem"
+        margin="2rem auto"
+        paddingY="4.2rem"
+        width="calc(100% - 3.2rem)"
+        minHeight="55.6rem"
+        maxWidth="auto"
+      >
+        <Heading
+          fontSize="4rem"
+          color="white"
+          textAlign="center"
+          fontWeight="300"
+          data-testid="title-page"
+        >
+          Formations
+        </Heading>
+
+        <Grid
+          templateColumns="repeat(auto-fill, 28.1rem)"
+          gap="2rem"
+          justifyContent="center"
+          maxWidth="175rem"
+          paddingY="3.2rem"
+          margin="auto"
+        ></Grid>
+      </Container>
+    </>
+  );
 };
 
-export default Dashboard;
+export default DashboardPage;
